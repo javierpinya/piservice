449a98e402151487f1e7d9e72eac8632623a754c
javierpinya <javierpinya@gmail.com>
2019-08-10 19:06:31 +0200

    añadido vehiculos_list.html

--- vehiculos/urls.py
+++ vehiculos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import VehiculoListView
+from .views import VehiculoListView, VehiculoDetailView
 
 vehiculos_patterns = ([
 		path('', VehiculoListView.as_view(), name='vehiculos'),
+		path('<int:pk>/<slug:slug>/', VehiculoDetailView.as_view(), name='vehiculo'),
 	], 'vehiculos')
--- vehiculos/views.py
+++ vehiculos/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Vehiculo
 
 class VehiculoListView(ListView):
+	model = Vehiculo
+
+class VehiculoDetailView(DetailView):
 	model = Vehiculo
